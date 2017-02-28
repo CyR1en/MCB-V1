08209e40105b4fa9a1e649f77aebba162b5182ed
Cyrien <ethanbacurio@yahoo.com>
2017-02-28 02:58:51 -0800

    Fix tab completion not completing in game players.

--- src/us/cyrien/MineCordBotV1/listeners/TabCompleteV2.java
+++ src/us/cyrien/MineCordBotV1/listeners/TabCompleteV2.java
@@ -3,6 +3,8 @@ package us.cyrien.MineCordBotV1.listeners;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.TabCompleteEvent;
@@ -16,32 +18,25 @@ public class TabCompleteV2 implements Listener {
     private MineCordBot mcb;
     private JDA jda;
     private List<String> tcList;
+    private ArrayList<String> players;
 
     public TabCompleteV2(MineCordBot mcb) {
         this.mcb = mcb;
         jda = mcb.getJda();
         tcList = mcb.getMcbConfig().getTextChannels();
+        players = new ArrayList<>();
     }
 
     @EventHandler
     public void onTabComplete(TabCompleteEvent e) {
         String[] buffers = e.getBuffer().split(" ");
         List<String> all = new ArrayList<>();
-        if(e.getBuffer().trim().endsWith("@")) {
+        if(buffers[buffers.length - 1].startsWith("@")) {
             for(String s : tcList){
                 TextChannel tc = jda.getTextChannelById(s);
                 if(tc != null)
                 for(Member m : tc.getMembers())
                     all.add("@" + m.getUser().getName().replaceAll(" ", "_"));
-            }
-            e.setCompletions(all);
-        } else {
-            for(String s : tcList){
-                TextChannel tc = jda.getTextChannelById(s);
-                if(tc != null)
-                    for(Member m : tc.getMembers())
-                        if(m.getUser().getName().startsWith((buffers[buffers.length - 1]).replaceAll("@", "")))
-                            all.add("@" + m.getUser().getName().replaceAll(" ", "_"));
             }
             e.setCompletions(all);
         }
